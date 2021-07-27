42eae76f27f7fb517dee65f465e12010dd8b4a82
chry003 <85602947+chry003@users.noreply.github.com>
2021-07-27 21:02:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
 var firebaseConfig = {
-	apiKey: "AIzaSyA2lJ0ZeRPklWR9DMucX01PN-9PHb1_CtU",
-	authDomain: "bugus-64626.firebaseapp.com",
-	projectId: "bugus-64626",
-	storageBucket: "bugus-64626.appspot.com",
-	messagingSenderId: "1080745234898",
-	appId: "1:1080745234898:web:42b3b87915130326ffb80b",
-	measurementId: "G-E9CT48Q4HS"
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
+	measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
